6925009a461d3e0ef39cb91ab0eadb363d896edb
osamasayd <osamasayd141@gmail.com>
2024-05-22 10:27:35 +0300

    set home page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+
+@section('title','Welcome')
+
+@section('contant')
+<div class="relative min-h-screen flex flex-col items-center justify-center w-full max-w-2xl px-6 lg:max-w-7xl">
+    <div class="flex lg:justify-center">
+        <h1>Welcome</h1>
+    </div>
+    <div class="mt-8 flex lg:justify-center bg-black">
+        THIS IS THE WELCOME PAGE
+    </div>
+</div>
+@endsection
